638b3b27a96c28ea2c4daa13de7a4ffe311261cc
Antti Norrkniivilä <antti.norrkniivila@aalto.fi>
2022-03-30 00:05:45 +0300

    small fixes

--- src/parts/CatList.tsx
+++ src/parts/CatList.tsx
@@ -23,14 +23,11 @@ const CatList: FC<SearchProps> = ({keyword}): JSX.Element => {
     const [pageCount, setPageCount] = useState<number>(1)
     const [currentPage, setCurrentPage] = useState<number>(1)
 
-    /*const [loadedImages, setLoadedImages] = useState<number>(0)
-    const [catsBeforeSearch, setCatsBeforeSearch] = useState<string[]>([])*/
     //helper function to make sure that fetched attribute exists, as in is not undefined
     const check = (atr: any): any => {
         return atr === undefined ?  '' : atr
     }
-    //fetches the json data from server
-    //data is saved to variable 'cats'
+    //fetches the json data from server, it is saved to 'cats'
     useEffect(() => {
         axios.get(url)
         .then((res: any) => {
@@ -67,24 +64,10 @@ const CatList: FC<SearchProps> = ({keyword}): JSX.Element => {
         .catch(error => console.log(error.response.status))
     }, [])
 
-
-    /*TODO: make loader work on search, idea:
-        1 save the cats of current page to variable
-        2 after searching, increment loadedImages for all
-        cats on current page that weren't there originally,
-        as in aren't in that variable
-        
-        loadedImages is incremented every time a new cat is rendered.
-        PROBLEMS:
-
-    */
-
-
-    //Chain of effects to render cat list to the screen. Executed as follows:
-    //1 filteredCats is set to contain the cats to show to user
-    //2 paginatedCats is set to divide them into chunks to view on one page
-    //3 pageCount is set to how many pages the data is divided to
-    
+    /*Chain of effects to render cat list to the screen. Executed as follows:
+    1 filteredCats is set to contain the cats to show to user
+    2 paginatedCats is set to divide them into chunks to view on one page
+    3 pageCount is set to how many pages the data is divided to */
     useEffect(() => {
         console.log('started filtering effect')
         const filtered: CatType[] = filterFunction(cats, keyword)
@@ -115,39 +98,6 @@ const CatList: FC<SearchProps> = ({keyword}): JSX.Element => {
         setPerPage(num)
     }
 
-    //Used to show cat cards only when all images are loaded
-    /*const catNotNew = (cat: CatType): boolean => {
-        return catsBeforeSearch.includes(cat.name)
-    }
-    useEffect(() => {
-        //saving the cats of current page to use when changing loadedImages
-        const nameArr: string[] = paginatedCats[currentPage - 1].map((cur: CatType): string => {
-            return cur.name
-        })
-        setCatsBeforeSearch(nameArr)
-        setLoadedImages(0)
-    },[keyword])
-    useEffect(() => {
-        //function to increment loadedImages
-        //init loadedImages to how many cats of current page were there originally
-        console.log('cats before search', catsBeforeSearch)
-        let newLoaded: number = 0
-        paginatedCats[currentPage - 1]
-            .forEach((cur: CatType) => { 
-                if (catNotNew(cur)) {
-                    newLoaded ++
-                    console.log(newLoaded)
-                }})
-        setLoadedImages(loadedImages + newLoaded)
-        console.log(catsBeforeSearch)
-        console.log(loadedImages)
-    },[paginatedCats])
-
-    const allLoaded = loadedImages === paginatedCats[currentPage - 1].length
-    const handleLoaded = () => {
-        setLoadedImages(loadedImages + 1)
-    }*/
-    
     const catPage = (
         paginatedCats[currentPage - 1].map((cur: CatType) => {
             return <CatItem key={cur.catId} catData={cur} />
